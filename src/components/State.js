50e6f51aa6ccf4a8e1d3166998226e599ab56ede
Trend20 <enockomondi305@gmail.com>
2020-04-21 14:06:04 -0400

    second commit

--- src/components/State.js
+++ src/components/State.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Lifecycle from './Lifecycle';
+import Name from './friendapp/Name';
 
 class State extends Component{
     constructor(props){
@@ -13,6 +14,7 @@ class State extends Component{
             <div className="state">
                 <p>Today is {this.state.date.toLocaleDateString()}</p>
                 <Lifecycle />
+                <Name />
             </div>
         )
     }
